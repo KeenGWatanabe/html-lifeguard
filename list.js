95a3ed10d3ae4ee1f15fca973d5292ce120ccf73
KeenGWatanabe <mckeenweb@gmail.com>
2026-03-14 13:50:21 +0800

    2manCPRchecklist

--- list.js
+++ list.js
@@ -26,8 +26,11 @@ else if (elementId === 'bm6') {
 else if (elementId === 'bm7') {
     window.open(href="list/bm7.pdf",'chat AI','resizable,height=300,width=500');
 }
-else if (elementId === '2-man CPR') {
+else if (elementId === '2-man CPR notes') {
     window.open(href="list/2-man CPR notes.pdf",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === '2-man CPR checklist') {
+    window.open(href="list/2-man CPR checklist.pdf",'chat AI','resizable,height=300,width=500');
 }
 else if (elementId === 'disclaimer') {
     alert(`This content is created for trainee and trained lifesaver's references only.  Context of saving methodology is subject to relevant skills, experiences and expertise interpretation, content here are by no means comprehensive enough to be used alone. The proper way to be an effective lifesaver is to undertake a course of training provided by a licensed trainer and certified by a recognised lifesaving authority. (chatbot developer)`);
