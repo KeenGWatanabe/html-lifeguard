300a64350ed00b24d030f11efc7fb371d710e93f
Rger <118863843+KeenGWatanabe@users.noreply.github.com>
2026-07-05 10:31:06 +0800

    Fix typo in SNR paper1 link

--- list.js
+++ list.js
@@ -33,7 +33,7 @@ else if (elementId === '2-man CPR notes') {
     window.open(href="list/2-man CPR notes.pdf",'chat AI','resizable,height=300,width=500');
 }    
 else if (elementId === 'SNR paper1') {
-    window.open(href="list/Senior Resuscitation Papaer 1.pdf",'chat AI','resizable,height=300,width=500');
+    window.open(href="list/Senior Resuscitation Paper 1.pdf",'chat AI','resizable,height=300,width=500');
 }
 else if (elementId === 'SNR paper2') {
     window.open(href="list/Senior Resuscitation Paper 2.pdf",'chat AI','resizable,height=300,width=500');
