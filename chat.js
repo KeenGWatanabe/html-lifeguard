6457e8810f23cd28804a24c91329bba07ac15a55
Rger <118863843+KeenGWatanabe@users.noreply.github.com>
2026-08-01 13:59:07 +0000

    Enhance lifesaving responses in chat.js
    Updated lifesaving responses and improved input processing logic.

--- chat.js
+++ chat.js
@@ -7,6 +7,8 @@ const keywordResponses = {
 	"goodbye" : "Goodbye! Have a great day!",
 	"unbelievable": "yes unbelievable",
 	"lifesaving1": "Reach, Throw, Wade, Accompanied Rescue 25m",
+	"lifesaving2": "airway, vomitting, recovery, throw rope, board tow 25m",
+	"lifesaving3": "sculling, thread, assisted landing, EAR shallow rescue, EAR 20m deep rescue, defense & release",
 	};
 function processInput() {
 	const userInput = document.getElementById("text_a").value;
@@ -20,9 +22,7 @@ for (const keyword in keywordResponses) {
 	if (input.includes(keyword)) {
 	response.textContent = keywordResponses[keyword];
 	return;
-}else if(input.toLowerCase().indexOf("lifesaving1")>-1){response.innerHTML=`Reach, Throw, Wade, Accompanied Rescue 25m`;
-	return;
-		
+c 		
 }else if(input.toLowerCase().indexOf("lss")>-1&&input.toLowerCase().indexOf("what")>-1){response.innerHTML=`SLSS - Singapore Life Saving Society<br>RLSS - Royal Life Saving Society<br>SLSAA - Surf Life Saving Association of Australia`;
 	return;
 }else if(input.toLowerCase().indexOf("how")>-1&&input.toLowerCase().indexOf("weather")>-1){response.innerHTML=`The weather today is <a href="https://www.nea.gov.sg/" target="_blank">NEA website</a>`;
