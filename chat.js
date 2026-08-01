2f81654f7591f6d36143408a56d089ee1d501f2a
Rger <118863843+KeenGWatanabe@users.noreply.github.com>
2026-08-01 13:39:04 +0000

    Improve keyword response logic in chat.js
    Refactor response handling for specific input keywords.

--- chat.js
+++ chat.js
@@ -20,7 +20,7 @@ for (const keyword in keywordResponses) {
 	if (input.includes(keyword)) {
 	response.textContent = keywordResponses[keyword];
 	return;
-}else if(input.toLowerCase().indexOf("ls1")>-1){response.innerHTML=`Reach, Throw, Wade, Accompanied Rescue 25m`;
+}else if(input.toLowerCase().indexOf("lifesaving1")>-1){response.innerHTML=`Reach, Throw, Wade, Accompanied Rescue 25m`;
 	return;
 		
 }else if(input.toLowerCase().indexOf("lss")>-1&&input.toLowerCase().indexOf("what")>-1){response.innerHTML=`SLSS - Singapore Life Saving Society<br>RLSS - Royal Life Saving Society<br>SLSAA - Surf Life Saving Association of Australia`;
